d23033f13c99532810ee169638fc1d7fb3dd6e25
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-03-05 22:38:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,8 @@
+﻿int N = int.Parse(Console.ReadLine()); 
+int M = int.Parse(Console.ReadLine()); 
+int T = int.Parse(Console.ReadLine());
+int minutes = N * 60 + M + T;
+int hours = (minutes / 60) % 24;
+int minuts = minutes % 60;
+string itog = $"{hours} : {minuts}";
+Console.WriteLine(itog);
